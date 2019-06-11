cfc33f85b227fc693c1cb7e0e4f3be5f4b9b3d7f
Krishna Teja Ayinala <teja.ayinala@gmail.com>
2019-06-11 18:21:19 -0500

    Updated processor implementation

--- ACA-TermProject/src/CacheProject/Processor.java
+++ ACA-TermProject/src/CacheProject/Processor.java
@@ -5,21 +5,49 @@ package CacheProject;
  *
  */
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedList;
 
-public class Processor {
+public class Processor extends CommonImpl {
 	public Queue queueProcessor = new Queue();
 	public Queue queueL1CtoProcessor = new Queue();
-	String testcaseFile = "";
+	String testcaseFile = "/Users/krishnatejaayinala/CSCI-8150-TermProject/ACA-TermProject/src/CacheProject/testcase.txt";
 
 	Processor() throws IOException {
-		 BufferedReader reader = new BufferedReader(new
-		 FileReader(testcaseFile));
-		// while (reader.readLine() != null) ;
-		// reader.close();
-		System.out.println("Processor Created");
+		BufferedReader reader = new BufferedReader(new FileReader(testcaseFile));
+		String test = null;
+		int insCount = 0;
+		while ((test = reader.readLine()) != null) {
+			String command = test;
+			int instructionNum = ++insCount;
+			String kind = test.split(" ")[0];
+			String address = test.split(" ")[1];
+			Address fAddress = formatAddress(Integer.parseInt(address), 6, 6, 5);
+			if (kind.equalsIgnoreCase("Read")) {
+				int instructionKind = 0;
+				String byteEnables = test.split(" ")[2];
+				ReadInstruction ins = new ReadInstruction();
+				ins.setCommand(command);
+				ins.setInstructioNum(instructionNum);
+				ins.setInstructionKind(instructionKind);
+				ins.setAddress(fAddress);
+				ins.setByteEnables(Integer.parseInt(byteEnables));
+				queueProcessor.enqueue(ins);
+			} else if (kind.equalsIgnoreCase("Write")) {
+				int instructionKind = 1;
+				char data = test.split("")[2].charAt(0);
+				WriteInstruction ins = new WriteInstruction();
+				ins.setCommand(command);
+				ins.setInstructioNum(instructionNum);
+				ins.setInstructionKind(instructionKind);
+				ins.setAddress(fAddress);
+				ins.setWriteData(data);
+				queueProcessor.enqueue(ins);
+			} else {
+				System.out.println("Inappropriate instruction format");
+			}
+		}
+		reader.close();
+		System.out.println("Processor DONE");
 	}
 }
