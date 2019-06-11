e3a0d3c84041aeffe0098544f36fa525fb63f3e4
Krishna Teja Ayinala <teja.ayinala@gmail.com>
2019-06-11 14:43:34 -0500

    Created address model

--- ACA-TermProject/src/CacheProject/Address.java
+++ ACA-TermProject/src/CacheProject/Address.java
@@ -8,9 +8,50 @@ package CacheProject;
  *
  */
 public class Address {
+	private String tag;
+	private String index;
+	private String offset;
+	private int address;
+	private char[] binaryAddress;
 
-	Address() {
+	public String getTag() {
+		return tag;
+	}
 
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getIndex() {
+		return index;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public String getOffset() {
+		return offset;
+	}
+
+	public void setOffset(String offset) {
+		this.offset = offset;
+	}
+
+	public int getAddress() {
+		return address;
+	}
+
+	public void setAddress(int address) {
+		this.address = address;
+	}
+
+	public char[] getBinaryAddress() {
+		return binaryAddress;
+	}
+
+	public void setBinaryAddress(char[] binaryAddress) {
+		this.binaryAddress = binaryAddress;
 	}
 
 }
