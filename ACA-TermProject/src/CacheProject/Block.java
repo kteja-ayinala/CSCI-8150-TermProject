6d9889813bd1ea24bdd9b3f24b2ba93f97757576
Krishna Teja Ayinala <teja.ayinala@gmail.com>
2019-06-11 19:14:34 -0500

    Update Block.java

--- ACA-TermProject/src/CacheProject/Block.java
+++ ACA-TermProject/src/CacheProject/Block.java
@@ -6,8 +6,65 @@ package CacheProject;
  */
 public class Block {
 
+	int validBit;
+	int dirtyBit;
+	int tag;
+	int offset;
+	char data[];
+
 	public Block() {
-		// TODO Auto-generated constructor stub
+		validBit = 1;
+		dirtyBit = 0;
+		tag = 0;
+		offset = 0;
+		data = new char[32];
 	}
 
+	public Block(char data[]) {
+		validBit = 1;
+		dirtyBit = 0;
+		tag = 0;
+		offset = 0;
+		data = new char[4];
+	}
+
+	public int getValidBit() {
+		return validBit;
+	}
+
+	public void setValidBit(int validBit) {
+		this.validBit = validBit;
+	}
+
+	public int getDirtyBit() {
+		return dirtyBit;
+	}
+
+	public void setDirtyBit(int dirtyBit) {
+		this.dirtyBit = dirtyBit;
+	}
+
+	public int getTag() {
+		return tag;
+	}
+
+	public void setTag(int tag) {
+		this.tag = tag;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
+	}
+
+	public char[] getData() {
+		return data;
+	}
+
+	public void setData(char[] data) {
+		this.data = data;
+	}
 }
